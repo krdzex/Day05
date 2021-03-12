de460ec92d3d48a42463c143745e8fbc60d77ac0
Krsto Kostic <kostic.krsto@gmail.com>
2021-03-13 00:34:04 +0100

    Adding ex for day05

---
+++ ex14/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "I have " + myNoun + " ,he always " + myVerb + ", he is really " + myAdjective + " and walking " + myAdverb + ".";
+    // Only change code above this line
+
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
